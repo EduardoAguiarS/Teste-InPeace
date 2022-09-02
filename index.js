8a451d99aa44706b849ecbfd1456034aba3c41a3
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-09-02 15:20:26 -0300

    title and path adjustments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Header = () => {
   const header = document.createElement("header");
   header.innerHTML = `
   <header class="header">
-    <img src="/assets/logo-uppersoft.png" alt="uppersoft" title="UpperSoft" class="logo">
+    <img src="../../assets/logo-uppersoft.png" alt="uppersoft" title="UpperSoft" class="logo">
   </header>
   `;
   return header;
