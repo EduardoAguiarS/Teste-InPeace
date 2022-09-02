aa75b5076672579b9797250535d8cb895319fbc8
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-09-01 21:09:44 -0300

    manage users

--- pages/ManageUsers/script.js
+++ pages/ManageUsers/script.js
@@ -1,21 +1,30 @@
+const userContainer = document.querySelector(".user__container");
+const container = document.querySelector(".container");
+
 async function getUser() {
   try {
     const response = await axios
-      .get("https://reqres.in/api/users?page=2")
-      .then(resp => resp.data.data);
-    response.map(user => {
-      const { id, first_name, last_name, email, avatar } = user;
-      const userRow = document.createElement("tr");
-      userRow.innerHTML = `
-        <td>${id}</td>
-        <td>${first_name}</td>
-        <td>${last_name}</td>
-        <td>${email}</td>
-        <td><img src="${avatar}" alt="${first_name} ${last_name}" /></td>
-        <td><button class="btn btn-danger btn-sm">Delete</button></td>
+      .get("https://reqres.in/api/users?page=1")
+      .then(resp => resp.data);
+    response.data.map(user => {
+      const { first_name, last_name, email, avatar } = user;
+      const userCard = document.createElement("div");
+      userCard.classList.add("user__card");
+      userCard.innerHTML = `
+        <img src="${avatar}" alt="user avatar" class="avatar"/>
+        <strong class="user__name">${first_name} ${last_name}</strong>
+        <span class="user__mail">${email}</span>
+        <button class="user__btn"><img src="../../assets/Icons/edit-icon.png" /></button>
       `;
-      document.body.appendChild(userRow);
+      userContainer.appendChild(userCard);
     });
+    const view = document.createElement("div");
+    view.classList.add("view");
+    view.innerHTML = `
+      <span>mostrando ${response.per_page} de ${response.total}</span>
+    `;
+    container.appendChild(view);
+    console.log(response);
   } catch (err) {
     console.log(err);
   }
